5681218c041f1969041ed9eb922e3208610f626e
akxaalex16 <akxaalex16@gmail.com>
2021-05-20 20:12:27 -0700

    Added GitHub link to project

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,17 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="Footer">
-      <p>Coded by Akxa Alex</p>
+      <p>
+        <a
+          href="https://github.com/akxaalex16/react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        {", "}
+        by Akxa Alex
+      </p>
     </div>
   );
 }
