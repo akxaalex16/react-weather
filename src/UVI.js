e2bce85fb96fac1c8618ebcf939fafc5d436f155
akxaalex16 <akxaalex16@gmail.com>
2021-06-06 02:02:38 -0700

    Updated UVI.js

--- src/UVI.js
+++ src/UVI.js
@@ -3,13 +3,29 @@ import "./UVI.css";
 
 export default function UVI(props) {
   let uvi = Math.round(props.uvi);
+  function changeUvi() {
+    if (uvi <= 2) {
+      return "Low";
+    }
+    if (uvi > 2 && uvi <= 5) {
+      return "Moderate";
+    }
+    if (uvi > 5 && uvi <= 7) {
+      return "High";
+    }
+    if (uvi > 7 && uvi <= 10) {
+      return "Very High";
+    } else {
+      return "Extreme";
+    }
+  }
   return (
     <div className="UVI">
       <div className="row">
         <div className="col-4">
           <div className="UV-Index">
             <div>UV Index</div>
-            {uvi}
+            {changeUvi()}
           </div>
         </div>
       </div>
