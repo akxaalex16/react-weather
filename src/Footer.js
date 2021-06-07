b8f53792be73e40fe111b10de495bb5da8c6bd33
akxaalex16 <akxaalex16@gmail.com>
2021-06-07 00:03:35 -0700

    Updated links in Footer.js and changed background around forecast icons in Forecast.css

--- src/Footer.js
+++ src/Footer.js
@@ -4,15 +4,24 @@ export default function Footer() {
   return (
     <div className="Footer">
       <p>
+        {" "}
+        This project is coded by Akxa Alex and is open-sourced on{" "}
         <a
           href="https://github.com/akxaalex16/react-weather"
           target="_blank"
           rel="noreferrer"
         >
-          Open-source code
+          GitHub
         </a>
-        {", "}
-        by Akxa Alex
+        <br />
+        and hosted on{" "}
+        <a
+          href="https://friendly-clarke-aa1415.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify{" "}
+        </a>
       </p>
     </div>
   );
